987eb0af95b6c13b5f7ca693bec87bceb7115a65
-Unknown- <mrunknownyt@gmail.com>
2021-11-13 02:13:07 -0700

    Shader rework. Allows for easier and more complex shaders + some GLH documentation

--- src/main/java/ulibs/gl/gl/GLH.java
+++ src/main/java/ulibs/gl/gl/GLH.java
@@ -8,27 +8,41 @@ import org.lwjgl.opengl.GL;
 import org.lwjgl.opengl.GL46;
 
 public class GLH {
+	/** GL True */
 	public static final int TRUE = GL46.GL_TRUE;
+	/** GL False */
 	public static final int FALSE = GL46.GL_FALSE;
 	
-	public static void readBuffer() {
-		GL46.glReadBuffer(GL46.GL_FRONT);
-	}
-	
+	/**
+	 * @param x Viewport's X
+	 * @param y Viewport's Y
+	 * @param w Viewport's Width
+	 * @param h Viewport's Height
+	 * @return {@link ByteBuffer} with viewport's pixel data
+	 */
 	public static ByteBuffer readPixels(int x, int y, int w, int h) {
+		GL46.glReadBuffer(GL46.GL_FRONT);
 		ByteBuffer buf = BufferUtils.createByteBuffer(w * h * 4);
 		GL46.glReadPixels(x, y, w, h, GL46.GL_RGBA, GL46.GL_UNSIGNED_BYTE, buf);
 		return buf;
 	}
 	
+	/** Unbinds the current texture **/
 	public static void unbindTexture() {
 		GL46.glBindTexture(GL46.GL_TEXTURE_2D, 0);
 	}
 	
+	/** Unbinds the current shader **/
+	public static void unbindShader() {
+		GL46.glUseProgram(0);
+	}
+	
+	/** Enables GL Scissors */
 	public static void enableScissors() {
 		GL46.glEnable(GL46.GL_SCISSOR_TEST);
 	}
 	
+	/** Disables GL Scissors */
 	public static void disableScissors() {
 		GL46.glDisable(GL46.GL_SCISSOR_TEST);
 	}
@@ -47,6 +61,7 @@ public class GLH {
 	}
 	
 	//TODO make this use an enum or something to make my life easier!
+	//future me here, what the fuck did i mean by use an enum?
 	public static int getError() {
 		return GL46.glGetError();
 	}
@@ -59,23 +74,33 @@ public class GLH {
 		GL46.glClear(GL46.GL_COLOR_BUFFER_BIT | GL46.GL_DEPTH_BUFFER_BIT);
 	}
 	
+	/** Sets the Viewport to the given values
+	 * @param x Viewport's X
+	 * @param y Viewport's Y
+	 * @param w Viewport's Width
+	 * @param h Viewport's Height
+	 */
 	public static void setViewport(int x, int y, int w, int h) {
 		GL46.glViewport(x, y, w, h);
 	}
 	
+	/** Enables GL Depth Test */
 	public static void enableDepth() {
 		GL46.glEnable(GL46.GL_DEPTH_TEST);
 	}
 	
+	/** Disables GL Depth Test */
 	public static void disableDepth() {
 		GL46.glDisable(GL46.GL_DEPTH_TEST);
 	}
 	
+	/** Enables GL Blend */
 	public static void enableBlend() {
 		GL46.glEnable(GL46.GL_BLEND);
 		GL46.glBlendFunc(GL46.GL_SRC_ALPHA, GL46.GL_ONE_MINUS_SRC_ALPHA);
 	}
 	
+	/** Disables GL Blend */
 	public static void disableBlend() {
 		GL46.glDisable(GL46.GL_BLEND);
 	}
@@ -84,11 +109,8 @@ public class GLH {
 		GL46.glActiveTexture(GL46.GL_TEXTURE0);
 	}
 	
+	/** @return The GL Version */
 	public static String getVersion() {
 		return GL46.glGetString(GL46.GL_VERSION);
-	}
-	
-	public static void unbindShader() {
-		GL46.glUseProgram(0);
 	}
 }
--- src/main/java/ulibs/gl/gl/Shader.java
+++ src/main/java/ulibs/gl/gl/Shader.java
@@ -1,6 +1,5 @@
 package main.java.ulibs.gl.gl;
 
-import java.awt.Color;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,7 +9,6 @@ import java.util.Map;
 
 import org.lwjgl.opengl.GL46;
 
-import main.java.ulibs.common.helpers.MathH;
 import main.java.ulibs.common.math.Vec2f;
 import main.java.ulibs.common.math.Vec3f;
 import main.java.ulibs.common.math.Vec4f;
@@ -19,67 +17,72 @@ import main.java.ulibs.common.utils.Console.WarningType;
 import main.java.ulibs.gl.math.Matrix4f;
 
 //TODO make better shader system! idk, make this abstract and add methods to make things easier
-
-public class Shader {
+public abstract class Shader {
 	public static final int VERTEX_ATTRIB = 0;
 	public static final int TCOORD_ATTRIB = 1;
 	
 	public final String name;
+	public final int id;
 	private final Matrix4f prMatrix;
-	private final int id;
+	
 	private final Map<String, Integer> locationCache = new HashMap<String, Integer>();
 	
-	private Matrix4f lastMatrix = Matrix4f.identity();
-	
-	public Shader(String name, Matrix4f prMatrix, String internalTitle) {
+	/**
+	 * @param name The name of the Shader
+	 * @param internalTitle The internal title package name
+	 * @param prMatrix The projection matrix to use
+	 */
+	public Shader(String name, String internalTitle, Matrix4f prMatrix) {
 		this.name = name;
+		this.id = load(name, internalTitle);
 		this.prMatrix = prMatrix;
-		id = load(name, internalTitle);
+		
 	}
 	
 	public void setup() {
 		bind();
-		set("pr_matrix", prMatrix);
-		if (GL46.glGetUniformLocation(id, "cl") != -1) {
-			set("cl", Shader.colorToVec4(Color.WHITE));
-		}
-		GL46.glUseProgram(0);
+		set("projection_matrix", prMatrix);
+		internalSetup();
+		GLH.unbindShader();
 	}
 	
+	/** Don't worry about binding/unbinding shader here! That's being handled elsewhere */
+	protected abstract void internalSetup();
+	
+	/** Binds the shader for use */
 	public void bind() {
 		GL46.glUseProgram(id);
 	}
 	
-	public void set(String name, int value) {
+	protected void set(String name, int value) {
 		GL46.glUniform1i(getUniform(name), value);
 	}
 	
-	public void set(String name, int[] value) {
+	protected void set(String name, int[] value) {
 		GL46.glUniform1iv(getUniform(name), value);
 	}
 	
-	public void set(String name, float value) {
+	protected void set(String name, float value) {
 		GL46.glUniform1f(getUniform(name), value);
 	}
 	
-	public void set(String name, Vec2f vec) {
+	protected void set(String name, Vec2f vec) {
 		GL46.glUniform2f(getUniform(name), vec.getX(), vec.getY());
 	}
 	
-	public void set(String name, Vec3f vec) {
+	protected void set(String name, Vec3f vec) {
 		GL46.glUniform3f(getUniform(name), vec.getX(), vec.getY(), vec.getZ());
 	}
 	
-	public void set(String name, Vec4f vec) {
+	protected void set(String name, Vec4f vec) {
 		GL46.glUniform4f(getUniform(name), vec.getX(), vec.getY(), vec.getZ(), vec.getW());
 	}
 	
-	public void set(String name, Matrix4f matrix) {
-		this.lastMatrix = matrix;
+	protected void set(String name, Matrix4f matrix) {
 		GL46.glUniformMatrix4fv(getUniform(name), false, matrix.toFloatBuffer());
 	}
 	
-	private int getUniform(String name) {
+	protected int getUniform(String name) {
 		if (locationCache.containsKey(name)) {
 			return locationCache.get(name);
 		}
@@ -102,14 +105,14 @@ public class Shader {
 		GL46.glShaderSource(fragID, frag);
 		
 		GL46.glCompileShader(vertID);
-		if (GL46.glGetShaderi(vertID, GL46.GL_COMPILE_STATUS) == GL46.GL_FALSE) {
+		if (GL46.glGetShaderi(vertID, GL46.GL_COMPILE_STATUS) == GLH.FALSE) {
 			Console.print(WarningType.Error, "Failed to compile vertex shader!");
 			Console.print(WarningType.Error, GL46.glGetShaderInfoLog(vertID));
 			return -1;
 		}
 		
 		GL46.glCompileShader(fragID);
-		if (GL46.glGetShaderi(fragID, GL46.GL_COMPILE_STATUS) == GL46.GL_FALSE) {
+		if (GL46.glGetShaderi(fragID, GL46.GL_COMPILE_STATUS) == GLH.FALSE) {
 			Console.print(WarningType.Error, "Failed to compile fragment shader!");
 			Console.print(WarningType.Error, GL46.glGetShaderInfoLog(fragID));
 			return -1;
@@ -141,18 +144,5 @@ public class Shader {
 		}
 		
 		return sb.toString();
-	}
-	
-	public static Vec4f colorToVec4(Color color) {
-		return new Vec4f(MathH.normalize(color.getRed(), 0, 255), MathH.normalize(color.getGreen(), 0, 255), MathH.normalize(color.getBlue(), 0, 255),
-				MathH.normalize(color.getAlpha(), 0, 255));
-	}
-	
-	public int getID() {
-		return id;
-	}
-	
-	public Matrix4f getLastMatrix() {
-		return lastMatrix;
 	}
 }
