cf6f1ff8ab3df461179e870dc543276be0619061
-Unknown- <mrunknownyt@gmail.com>
2021-11-17 04:47:27 -0700

    Update ZConstant.java

--- src/main/java/ulibs/gl/gl/ZConstant.java
+++ src/main/java/ulibs/gl/gl/ZConstant.java
@@ -8,9 +8,9 @@ public class ZConstant {
 	public static final ZConstant Z_WORLD_OVERLAY = new ZConstant();
 	public static final ZConstant Z_HUD_BACKGROUND = new ZConstant();
 	public static final ZConstant Z_HUD_BASE = new ZConstant();
-	public static final ZConstant Z_HUD_DETAIL0 = new ZConstant();
-	public static final ZConstant Z_HUD_DETAIL1 = new ZConstant();
-	public static final ZConstant Z_HUD_DETAIL2 = new ZConstant();
+	public static final ZConstant Z_HUD_DETAIL_0 = new ZConstant();
+	public static final ZConstant Z_HUD_DETAIL_1 = new ZConstant();
+	public static final ZConstant Z_HUD_DETAIL_2 = new ZConstant();
 	public static final ZConstant Z_HUD_TEXT = new ZConstant();
 	public static final ZConstant Z_HUD_MOUSE = new ZConstant();
 	public static final ZConstant Z_HUD_MOUSE_TEXT = new ZConstant();
@@ -22,5 +22,9 @@ public class ZConstant {
 	private ZConstant() {
 		this.z = zz;
 		zz += 0.1f;
+	}
+	
+	public ZConstant(float z) {
+		this.z = z;
 	}
 }
